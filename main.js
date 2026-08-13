896613a8a3c80ee569e9198209d265344e81b04c
FRANCISCA LEONE FRUTUOSO FERREIRA <francisca_ferreir151@estudante.sesisenai.org.br>
2026-08-12 22:01:38 -0300

    revisaojs

---
+++ main.js
@@ -0,0 +1,8 @@
+import exemplosVariaveis from"./scripts/variaveis.js";
+
+exemplosVariaveis()
+
+ const btnVariaveis = document.getElementById("btnVariaveis");
+ btnVariaveis.addEventListener("click", exemplosVariaveis);
+
+
